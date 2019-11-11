a3a6986b96d4b2ec52db9cd35bbb8ebec9736e1f
Schmoul622 <55683231+Schmoul622@users.noreply.github.com>
2019-11-11 14:15:41 +0100

    commentaire samuel

--- EPSIC_Bataille_Navale/Controllers/SetupController.cs
+++ EPSIC_Bataille_Navale/Controllers/SetupController.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 namespace EPSIC_Bataille_Navale.Controllers
 {
     public class SetupController
-    {
+    { // Déclaration des propriétées
         private Setup view = null;
         public Grid grid;
         public string playerName = "";
@@ -15,6 +15,7 @@ namespace EPSIC_Bataille_Navale.Controllers
 
         public SetupController(Setup view, int size)
         {
+            // Création d'une nouvelle grille
             this.view = view;
             grid = new Grid(size);
         }
@@ -27,104 +28,106 @@ namespace EPSIC_Bataille_Navale.Controllers
                 for (int i = 0; i < grid.boatsList.Count; i++)
                 {
                     // --- RIGHT ---
-
+                    // Possiblité de placement à droite
                     bool possible = true;
-                    for (int j = 0; j < grid.boatsList[i]; j++)
+                    for (int j = 0; j < grid.boatsList[i]; j++) // Parcours la liste des bateaux à placer
                     {
-                        if (x + j >= grid.grid.GetLength(0))
+                        if (x + j >= grid.grid.GetLength(0)) // Empêche le bateau de sortir de la grille
                         {
                             possible = false;
                             break;
                         }
-                        if (grid.grid[x + j, y].boat != null)
+                        if (grid.grid[x + j, y].boat != null) // Vérifie si un bateau n'est pas déjà là
                         {
-                            possible = false;
+                            possible = false; // Si un bateau est déjà là on n'a pas la possibilité de placer ici
                         }
                     }
                     if (possible)
                     {
-                        possibleCells.Add(new int[] { x + grid.boatsList[i] - 1, y });
+                        possibleCells.Add(new int[] { x + grid.boatsList[i] - 1, y }); // La cellule devient verte 
                     }
 
                     // --- LEFT ---
-
+                    // Possibilité de placement à gauche
                     possible = true;
-                    for (int j = 0; j < grid.boatsList[i]; j++)
+                    for (int j = 0; j < grid.boatsList[i]; j++) // Parcours la liste des bateaux à placer
                     {
                         if (x - j < 0)
                         {
                             possible = false;
                             break;
                         }
-                        if (grid.grid[x - j, y].boat != null)
+                        if (grid.grid[x - j, y].boat != null) // Vérifie si un bateau n'est pas déjà là
                         {
                             possible = false;
                         }
                     }
                     if (possible)
                     {
-                        possibleCells.Add(new int[] { x - grid.boatsList[i] + 1, y });
+                        possibleCells.Add(new int[] { x - grid.boatsList[i] + 1, y }); // La cellule devient verte 
                     }
 
                     // --- DOWN ---
-
+                    // Possiblité de placement en haut
                     possible = true;
-                    for (int j = 0; j < grid.boatsList[i]; j++)
+                    for (int j = 0; j < grid.boatsList[i]; j++) // Parcours la liste des bateaux à placer
                     {
-                        if (y + j >= grid.grid.GetLength(1))
+                        if (y + j >= grid.grid.GetLength(1)) // Empêche le bateau de sortir de la grille
                         {
                             possible = false;
                             break;
                         }
-                        if (grid.grid[x, y + j].boat != null)
+                        if (grid.grid[x, y + j].boat != null) // Vérifie si un bateau n'est pas déjà là
                         {
                             possible = false;
                         }
                     }
                     if (possible)
                     {
-                        possibleCells.Add(new int[] { x, y + grid.boatsList[i] - 1 });
+                        possibleCells.Add(new int[] { x, y + grid.boatsList[i] - 1 }); // La cellule devient verte
                     }
 
                     // --- UP ---
-
+                    // Possiblité de placement en bas
                     possible = true;
-                    for (int j = 0; j < grid.boatsList[i]; j++)
+                    for (int j = 0; j < grid.boatsList[i]; j++) // Parcours la liste des bateaux à placer
                     {
                         if (y - j < 0)
                         {
                             possible = false;
                             break;
                         }
-                        if (grid.grid[x, y - j].boat != null)
+                        if (grid.grid[x, y - j].boat != null) // Vérifie si un bateau n'est pas déjà là
                         {
                             possible = false;
                         }
                     }
                     if (possible)
                     {
-                        possibleCells.Add(new int[] { x, y - grid.boatsList[i] + 1 });
+                        possibleCells.Add(new int[] { x, y - grid.boatsList[i] + 1 }); // La cellule devient verte
                     }
                 }
             }
             else
             {
-                bool validClick = false;
-                foreach (int[] cell in possibleCells)
+                bool validClick = false; // Initilisation de la variable "validClick"
+                foreach (int[] cell in possibleCells) // Parcous toutes les cellules vertes
                 {
-                    if (x == cell[0] && y == cell[1])
+                    if (x == cell[0] && y == cell[1]) // Si la cellule cliquée est dans les cellules vertes
                     {
                         validClick = true;
                         break;
                     }
                 }
-                if (validClick)
+                if (validClick) // Si validClick est égal à "true" (donc si la cellule cliquée est dans les cellules vertes)
                 {
+                    // Aide la boucle for en dessous
                     int minX = Math.Min(x, clickedCell[0]);
                     int maxX = Math.Max(x, clickedCell[0]);
                     int minY = Math.Min(y, clickedCell[1]);
                     int maxY = Math.Max(y, clickedCell[1]);
-                    Boat boat = new Boat();
+                    Boat boat = new Boat(); // Crée un bateau vide
+                    // Met un bateau dans chaque cellules verte selectionné
                     for (int i = minX; i <= maxX; i++)
                     {
                         for (int j = minY; j <= maxY; j++)
@@ -133,7 +136,7 @@ namespace EPSIC_Bataille_Navale.Controllers
                             grid.grid[i, j].boat = boat;
                         }
                     }
-                    grid.boats.Add(boat);
+                    grid.boats.Add(boat); // Ajoute la bateau dans la grille
                     grid.boatsList.Remove(boat.length);
                     view.EnableCancelButton(true);
                     if (grid.boatsList.Count == 0)
@@ -147,6 +150,7 @@ namespace EPSIC_Bataille_Navale.Controllers
             view.RefreshGrid(grid, clickedCell, possibleCells);
         }
 
+        // Méthode pour supprimer les bateau qu'on vient de placer 
         public void DeleteLastBoat()
         {
             if(grid.boats.Count > 0)
@@ -173,15 +177,16 @@ namespace EPSIC_Bataille_Navale.Controllers
             }
         }
 
+        // Méthodes qui place les bateaux de l'IA
         public void AIChoise()
         {
-            playerName = "L'IA";
+            playerName = "L'IA"; 
 
-            Random random = new Random();
+            Random random = new Random(); // Variable random
             while(grid.boatsList.Count > 0)
             {
-                int x = random.Next(0, grid.grid.GetLength(0) - 1);
-                int y = random.Next(0, grid.grid.GetLength(1) - 1);
+                int x = random.Next(0, grid.grid.GetLength(0) - 1); // Choisi une case random dans x
+                int y = random.Next(0, grid.grid.GetLength(1) - 1); // Choisi une case random dans y
                 Click(x, y);
                 if(possibleCells.Count > 0)
                 {
--- EPSIC_Bataille_Navale/Models/Cell.cs
+++ EPSIC_Bataille_Navale/Models/Cell.cs
@@ -1,8 +1,9 @@
-﻿
-namespace EPSIC_Bataille_Navale.Models
+﻿namespace EPSIC_Bataille_Navale.Models
 {
+    // Classe qui définie l'état des cellules
     public enum State
     {
+        // Différent type d'état pour les cellules
         noActivity,
         noBoat,
         boat,
@@ -10,6 +11,7 @@ namespace EPSIC_Bataille_Navale.Models
         invalid
     }
 
+    // Création de la classe "Cell"
     public class Cell
     {
         public State state = State.noActivity;
